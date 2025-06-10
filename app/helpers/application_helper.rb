83e8cd482e6f97a3758009aa041df04a8966c554
Naodab <nguyenhobadoan110304@gmail.com>
2025-06-11 01:08:17 +0700

    Style(home): style home page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def full_title(title = "")
+    title.blank? ? "TaskFlow" : "#{title} | TaskFlow"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  root "static_pages#home"
+  resources :static_pages, part: "", only: [] do
+    collection do
+      get "/about", to: "static_pages#about"
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
