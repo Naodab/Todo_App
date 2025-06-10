7926870322782c140d55f9de75ac35c774432ca5
Naodab <nguyenhobadoan110304@gmail.com>
2025-06-10 20:05:23 +0700

    Chore(database): setup MySQL config for production environment

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module TodoApp
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
+    config.assets.enable = true
+    config.assets.css_compressor = :sassc
 
     # Configuration for the application, engines, and railties goes here.
     #
